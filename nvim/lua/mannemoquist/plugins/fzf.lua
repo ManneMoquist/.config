c1d8f18b0b2340ead1a280925a591c1c5d5e2b08
Manne Moquist <manne.moquist@readly.com>
2025-02-26 10:30:32 +0100

    add fzf fuzzy finder

---
+++ nvim/lua/mannemoquist/plugins/fzf.lua
@@ -0,0 +1,15 @@
+return {
+  "ibhagwan/fzf-lua",
+  --dependencies = { "nvim-tree-nvim-web-devicons" },
+  opts = {},
+  config = function()
+    local keymap = vim.keymap
+    local fzf = require("fzf-lua")
+
+    keymap.set("n", "<leader>ff", fzf.files, { desc = "Fuzzy find files" })
+    keymap.set("n", "<leader>fg", fzf.grep_project, { desc = "Fuzzy grep over files in project" })
+    keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Fuzzy find in buffers" })
+    keymap.set("n", "<leader>fc", fzf.builtin, { desc = "Fuzzy find all" })
+    keymap.set("n", "<leader>fl", fzf.lsp_finder, { desc = "Fuzzy find lsp-references" })
+  end
+}
