59e881cd8847483bbdd87e9c87abdfe094f3b73f
Manne Moquist <manne.moquist@readly.com>
2025-03-26 14:35:39 +0100

    diagnostics

--- nvim/lua/mannemoquist/plugins/fzf.lua
+++ nvim/lua/mannemoquist/plugins/fzf.lua
@@ -9,6 +9,7 @@ return {
     keymap.set("n", "<leader>ff", fzf.files, { desc = "Fuzzy find files" })
     keymap.set("n", "<leader>fg", fzf.grep_project, { desc = "Fuzzy grep over files in project" })
     keymap.set("n", "<leader>fb", fzf.buffers, { desc = "Fuzzy find in buffers" })
+    keymap.set("n", "<leader>fd", fzf.lsp_workspace_diagnostics, { desc = "LSP diagnostics workspace" })
     keymap.set("n", "<leader>fc", fzf.builtin, { desc = "Fuzzy find all" })
     keymap.set("n", "<leader>fl", fzf.lsp_finder, { desc = "Fuzzy find lsp-references" })
   end
